a6721960762b04510bc44e46f55b0b1bc050f0e8
xlbs <928135874@qq.com>
2018-03-13 22:45:50 +0800

    修改

--- Distribution/ServiceInterface/src/com/xlbs/serviceinterface/serviceconstant/ServiceParamter.java
+++ Distribution/ServiceInterface/src/com/xlbs/serviceinterface/serviceconstant/ServiceParamter.java
@@ -2,6 +2,14 @@ package com.xlbs.serviceinterface.serviceconstant;
 
 public class ServiceParamter {
 
-    public final static String MICRO_SERVICE = "MicroService";
+    /**
+     * 内存库中集群服务的地址
+     */
+    public final static String Cluster_Service = "ClusterService";
+
+    /**
+     * R集群服务
+     */
+    public final static String RPC_ClusterService = "RpcClusterService";
 
 }
