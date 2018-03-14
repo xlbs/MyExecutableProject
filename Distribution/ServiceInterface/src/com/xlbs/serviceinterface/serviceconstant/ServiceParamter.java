92842375b818c76ee013a1bf86a5f8f63dcc6bd6
xielbs <928135874@qq.com>
2018-03-14 16:16:30 +0800

    修改

--- Distribution/ServiceInterface/src/com/xlbs/serviceinterface/serviceconstant/ServiceParamter.java
+++ Distribution/ServiceInterface/src/com/xlbs/serviceinterface/serviceconstant/ServiceParamter.java
@@ -3,12 +3,16 @@ package com.xlbs.serviceinterface.serviceconstant;
 public class ServiceParamter {
 
     /**
+     * 集群主节点
+     */
+    public  final static String Cluster_Leader = "ClusterLeader";
+    /**
      * 内存库中集群服务的地址
      */
     public final static String Cluster_Service = "ClusterService";
 
     /**
-     * R集群服务
+     * RPC集群服务
      */
     public final static String RPC_ClusterService = "RpcClusterService";
 
